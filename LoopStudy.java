efea32cbdcf4a79c067051d53c6bc00f25232b98
9534495797 <chaubeyankit927@gmail.com>
2024-06-13 07:33:07 +0530

    ok

---
+++ LoopStudy.java
@@ -0,0 +1,20 @@
+public class LoopStudy {
+    public static void main(String[] args) {
+        System.out.println("for loop");
+        for(int i=0;i<100;i++){
+            System.out.println(i);
+        }
+        System.out.println("while loop");
+        int i=1;
+        while(i<10){
+            System.out.println("Hello");
+            i++;
+        }
+        System.out.println("do while loop");
+        int n=4;
+        do{
+            System.out.println(n);
+            n--;
+        }while(n>5);
+    }
+}

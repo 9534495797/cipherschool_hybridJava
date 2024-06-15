03c1909be7daef5671fd0a1caf68df8335f50c75
9534495797 <chaubeyankit927@gmail.com>
2024-06-15 07:14:28 +0530

    ok

---
+++ Students.java
@@ -0,0 +1,24 @@
+public class Students {
+    int roll;
+    String name;
+public static void main(String[] args) {
+    Students s1=new Students();
+    Students s2=new Students();
+    Students s3=new Students();
+s1.name="Ram";
+s1.roll=1;
+s2.name="shyam";
+s2.roll=2;
+s3.name="ram";
+s3.roll=1;
+System.out.println(s1.name);
+System.out.println(s1.roll);
+
+
+System.out.println(s2.name);
+System.out.println(s2.roll);
+
+System.out.println(s3.name);
+System.out.println(s3.roll);
+}
+}

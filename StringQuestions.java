58c1334e2ac1888c91427c9b21ac56584d2525ed
9534495797 <chaubeyankit927@gmail.com>
2024-07-01 07:29:46 +0530

    cipherschool lect-28

---
+++ StringQuestions.java
@@ -0,0 +1,17 @@
+public class StringQuestions {
+//     public static void main(String[] args) {
+//         //reverse
+//         String s1="Ankit";
+//         int n=s1.length();
+//         char arr[]=new arr[n];
+// for(int i=0;i<n;i++){
+//     arr[i]=s1.charAt(n-i-1);
+
+// }
+// String rev=new String(arr);
+// System.out.println(rev);
+//     }
+
+//palindrome
+
+}
